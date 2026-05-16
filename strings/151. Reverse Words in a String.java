27e1beecfafd497169ddce440b20c0f3368c6935
Nitish <727823tuee108@skct.edu.in>
2026-05-16 09:41:00 +0530

    Updated strings and arrays

---
+++ strings/151. Reverse Words in a String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] str=s.split("\\s+");
+        StringBuilder sb=new StringBuilder();
+        for(int i=str.length-1;i>=0;i--){
+            sb.append(str[i]+" ");
+        }
+        return sb.toString().trim();
+    }
+}
